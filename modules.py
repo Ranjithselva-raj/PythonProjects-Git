1185df51b3a2c5f79ec22476d26a3fec9bab751f
Ranjith Selvaraj <ranjee1691@gmail.com>
2023-06-23 13:16:58 +0100

    Practice on package creation added

---
+++ modules.py
@@ -0,0 +1,55 @@
+def test(a,b):
+        
+        """
+    Takes in two parameters, `a` and `b`, and returns their sum.
+
+    Args:
+    - a (int or float): First number to be added.
+    - b (int or float): Second number to be added.
+
+    Returns:
+    - int or float: The sum of `a` and `b`.
+    """
+        return a+b
+
+
+def multiply(a,b):
+        """
+    Takes in two parameters, `a` and `b`, and returns their product.
+
+    Args:
+    - a (int or float): First number to be multiplied.
+    - b (int or float): Second number to be multiplied.
+
+    Returns:
+    - int or float: The product of `a` and `b`.
+    """
+        return a*b
+
+def divide(a,b):
+        """
+    Takes in two parameters, `a` and `b`, and returns their quotient.
+
+    Args:
+    - a (int or float): First number to be divided.
+    - b (int or float): Second number to be divided.
+
+    Returns:
+    - int or float: The quotient of `a` and `b`.
+    """
+        return a/b
+
+def subtract(a,b):
+        """
+    Takes in two parameters, `a` and `b`, and returns their difference.
+
+    Args:
+    - a (int or float): First number to be subtracted.
+    - b (int or float): Second number to be subtracted.
+
+    Returns:
+    - int or float: The difference of `a` and `b`.
+    """
+        return a-b
+
+
---
+++ my_module.py
@@ -0,0 +1,11 @@
+
+data ={'name':'ineuron',
+       'course':['python','machine learning','deep learning'],
+       'greeting':'Greeting from ineuron'}
+
+def get_course():
+    return data['course']
+
+def greeting():
+    return data['greeting']
+
